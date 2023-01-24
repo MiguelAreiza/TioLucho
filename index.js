fb946604013174650d99a461b529622e64a63bb4
MiguelAreiza <miangelareiza@gmail.com>
2023-01-24 18:57:05 -0500

    Test deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready( () => {
+
+    Dynamsoft.DBR.BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+    let scanner = null;
+    (async()=>{
+        scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+        scanner.onFrameRead = results => {console.log(results);};
+        scanner.onUnduplicatedRead = (txt, result) => {};
+        await scanner.show();
+    })();
+
+})
