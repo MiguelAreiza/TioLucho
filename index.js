5506866cd334887a0f2a6a1ee1045a320eb6fe4d
MiguelAreiza <miangelareiza@gmail.com>
2023-01-24 19:42:33 -0500

    test deploy

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 
 $(document).ready( () => {
 
-    Dynamsoft.DBR.BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
-    let scanner = null;
-    (async()=>{
-        scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
-        scanner.onFrameRead = results => {console.log(results);};
-        scanner.onUnduplicatedRead = (txt, result) => {};
-        await scanner.show();
-    })();
+    function onScanSuccess(qrCodeMessage) {
+        document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+    }
+
+    function onScanError(errorMessage) {
+      //handle scan error
+    }
+
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader", { 
+            fps: 10, 
+            qrbox: 250 
+        }
+    );
+
+    html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+    $("#reader > div:nth-child(1)").hide();
+    $("#reader__dashboard_section_csr > div > button").click();
 
 })
